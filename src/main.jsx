58498c687b72d71aff93de70ecb699e948f01a65
Aditya-jod <aditya1710j@gmail.com>
2026-02-11 21:39:50 +0530

    Initial prototype: Joddex website

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // Cleared: placeholder main entry
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './app.jsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
